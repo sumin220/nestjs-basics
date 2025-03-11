c2e5cef8ab29eee081514aabd9760c90731b8c6c
sumin <sonsumin00@gmail.com>
2025-03-11 12:50:04 +0900

    :sparkles: [Feat] : IoC 컨테이너

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,29 @@
 import { Injectable } from '@nestjs/common';
 
+export interface Movie {
+  id: number;
+  title: string;
+}
+
+// 클래스를 알아서 IoC 컨테이너에서 관리를 해줌
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  private movies: Movie[] = [
+    {
+      id: 1,
+      title: '해리포터',
+    },
+    {
+      id: 2,
+      title: '반지의 제왕',
+    },
+  ];
+
+  getManyMovies(title?: string) {
+    if (!title) {
+      return this.movies;
+    }
+
+    return this.movies.filter((m) => m.title.startsWith(title));
   }
 }
