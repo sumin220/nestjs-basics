71276176f03c0b704aa0895607af79c8a1aac384
sumin <sonsumin00@gmail.com>
2025-03-16 22:40:56 +0900

    :sparkles: [Feat] : movie module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MovieModule } from './movie/movie.module';
 
 @Module({
-  imports: [],
-  exports: [AppService],
+  imports: [MovieModule],
   controllers: [AppController],
   providers: [AppService],
 })
