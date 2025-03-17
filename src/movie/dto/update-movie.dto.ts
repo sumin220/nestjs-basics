04678662a17b46378d3e54f2125af035236c4c70
sumin <sonsumin00@gmail.com>
2025-03-17 16:41:35 +0900

    :sparkles: [Feat] : Custom Validator

--- src/movie/dto/update-movie.dto.ts
+++ src/movie/dto/update-movie.dto.ts
@@ -1,19 +1,68 @@
 import {
   Contains,
-  Equals, IsAlphanumeric, IsArray,
-  IsBoolean, IsCreditCard, IsDateString,
-  IsDefined, IsDivisibleBy,
-  IsEmpty, IsEnum, IsHexColor,
-  IsIn, IsInt, IsLatLong, IsNegative,
+  Equals,
+  IsAlphanumeric,
+  IsArray,
+  IsBoolean,
+  IsCreditCard,
+  IsDateString,
+  IsDefined,
+  IsDivisibleBy,
+  IsEmpty,
+  IsEnum,
+  IsHexColor,
+  IsIn,
+  IsInt,
+  IsLatLong,
+  IsNegative,
   IsNotEmpty,
-  IsNotIn, IsNumber,
-  IsOptional, IsPositive, IsString, IsUUID, Max, MaxLength, Min, MinLength, NotContains,
-  NotEquals,
+  IsNotIn,
+  IsNumber,
+  IsOptional,
+  IsPositive,
+  IsString,
+  IsUUID,
+  Max,
+  MaxLength,
+  Min,
+  MinLength,
+  NotContains,
+  NotEquals, registerDecorator, Validate,
+  ValidationArguments, ValidationOptions, ValidatorConstraint,
+  ValidatorConstraintInterface,
 } from 'class-validator';
 
 enum MovieGenre {
   Fantasy = 'fantasy',
   Action = 'action',
+}
+
+@ValidatorConstraint({
+  async: true,
+})
+class PasswordValidator implements ValidatorConstraintInterface {
+  validate(
+    value: any,
+    validationArguments?: ValidationArguments,
+  ): Promise<boolean> | boolean {
+    //비밀번호 길이는 4~8
+    return value.length > 4 && value.length < 8;
+  }
+
+  defaultMessage?(validationArguments?: ValidationArguments): string {
+    return '비밀번호의 길이는 4~8자 여야 합니다. 입력된 비밀번호 : ($value)'
+  }
+}
+
+function IsPasswordValid(validationOptions?: ValidationOptions) {
+  return function (object: Object, propertyName: string) {
+    registerDecorator({
+      target: object.constructor,
+      propertyName,
+      options: validationOptions,
+      validator: PasswordValidator
+    });
+  };
 }
 
 export class UpdateMovieDto {
@@ -53,6 +102,8 @@ export class UpdateMovieDto {
   // @MaxLength(5)
   // @MinLength(2)
   // @IsUUID()
-  @IsLatLong() //위도 경도
+  // @IsLatLong() //위도 경도
+
+  @IsPasswordValid({message: "다른메세지"})
   test: string;
 }
