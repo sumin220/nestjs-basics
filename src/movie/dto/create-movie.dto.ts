0b3c0ce2408b5023c100132b5c00feb667f08bef
sumin <sonsumin00@gmail.com>
2025-03-17 15:54:18 +0900

    :sparkles: [Feat] : class-validator

--- src/movie/dto/create-movie.dto.ts
+++ src/movie/dto/create-movie.dto.ts
@@ -1,5 +1,9 @@
+import { IsNotEmpty } from 'class-validator';
+
 export class CreateMovieDto {
+  @IsNotEmpty({message: '제목은 비어있으면 안 됩니다.'})
   title: string;
 
+  @IsNotEmpty({message: '제목은 비어있으면 안 됩니다.'})
   genre: string;
 }
