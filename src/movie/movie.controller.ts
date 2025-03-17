7ca519480154680beb69f777b360072352ca29bc
sumin <sonsumin00@gmail.com>
2025-03-17 15:33:36 +0900

    :sparkles: [Feat] : import dto

--- src/movie/movie.controller.ts
+++ src/movie/movie.controller.ts
@@ -9,6 +9,8 @@ import {
   Query,
 } from '@nestjs/common';
 import { MovieService } from './movie.service';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
 
 @Controller('movie')
 export class MovieController {
@@ -26,13 +28,16 @@ export class MovieController {
   }
 
   @Post()
-  postMovie(@Body('title') title: string) {
-    return this.movieService.createMovie(title);
+  postMovie(@Body() body: CreateMovieDto) {
+    return this.movieService.createMovie(body);
   }
 
   @Patch(':id')
-  patchMovie(@Param('id') id: string, @Body('title') title: string) {
-    return this.movieService.updateMovie(+id, title);
+  patchMovie(
+    @Param('id') id: string,
+    @Body() body: UpdateMovieDto
+  ) {
+    return this.movieService.updateMovie(+id, body);
   }
 
   @Delete(':id')
