bda18f53007917a54089052cd3ae7644bd4d5013
sumin <sonsumin00@gmail.com>
2025-03-18 13:04:45 +0900

    :sparkles: [Feat] : 환경변수 사용

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,22 @@
 import { Module } from '@nestjs/common';
 import { MovieModule } from './movie/movie.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import * as process from 'node:process';
 
 @Module({
-  imports: [MovieModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: process.env.DB_TYPE as "postgres",
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT || '5432', 10),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [],
+      synchronize: true, // 개발할 때만 true
+    }),
+    MovieModule],
 })
 export class AppModule {}
