ab939e42b5f3a678254be0fe328b39e4cd0a64a9
sumin <sonsumin00@gmail.com>
2025-03-19 13:40:08 +0900

    :sparkles: [Feat] : Movie 엔티티

--- src/movie/entity/movie.entity.ts
+++ src/movie/entity/movie.entity.ts
@@ -1,13 +1,22 @@
-import { Exclude, Expose, Transform } from 'class-transformer';
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn, VersionColumn } from 'typeorm';
 
+@Entity()
 export class Movie {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
   title: string;
 
-  @Transform(
-    ({value}) => value.toString().toUpperCase()
-  )
+  @Column()
   genre: string;
 
+  @CreateDateColumn()
+  createdAt: Date;
 
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @VersionColumn()
+  version: number;
 }
