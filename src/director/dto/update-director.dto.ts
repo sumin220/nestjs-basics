15fa670de82660f66a08e5d22a9899479de94863
sumin <sonsumin00@gmail.com>
2025-04-09 12:12:32 +0900

    :sparkles: [Feat] : director dto

--- src/director/dto/update-director.dto.ts
+++ src/director/dto/update-director.dto.ts
@@ -1,15 +1,19 @@
-import { IsDateString, IsNotEmpty } from 'class-validator';
+import { IsDateString, IsNotEmpty, IsOptional } from 'class-validator';
 
 export class UpdateDirectorDto {
 
   @IsNotEmpty()
+  @IsOptional()
   name?: string;
 
   @IsNotEmpty()
   @IsDateString()
+  @IsOptional()
+
   dob?: Date;
 
   @IsNotEmpty()
+  @IsOptional()
   nationality?: string;
 
 }
